a5548a162fee2b0aa3ed2e93e7f57385e6479564
nnekasandra <nnekasandra@gmail.com>
2022-04-07 18:30:10 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        sky: "#75B1A9",
+        sunglow: "#D9B44A",
+        mountain: "#4F6457",
+        mist: "#ACD0C0",
+        "blue-black": "#021C1E",
+        "cadet-blue": "#004445",
+        rain: "#2C7873",
+        greenery: "#6FB98F",
+        "dim-white": "#ccc",
+      },
+    },
+    screens: {
+      md: "900px",
+      lg: "1200px",
+    },
+  },
+  plugins: [],
+};
